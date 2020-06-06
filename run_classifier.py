3988f9f3c8114376822b97caf69725638615b22b
Merishna Singh Suwal <merishna97@gmail.com>
2020-06-06 13:52:57 +0545

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -490,7 +490,7 @@ def file_based_convert_examples_to_features(
                                      max_seq_length, tokenizer)
 
     def create_int_feature(values):
-      f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+      f = tf.compat.v1.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
       return f
 
     features = collections.OrderedDict()
